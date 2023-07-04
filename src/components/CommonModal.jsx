d257cda9c985f8ab3feddf3988cfeb8d2854ba79
Amir Khan <amir.khan@wolkus.com>
2023-07-04 15:07:04 +0530

    PRD-1 added commong modal and userprofile

---
+++ src/components/CommonModal.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '100vw',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+};
+
+const previewImageStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'transparent',
+  border: '2px transparent',
+  borderRadius: '50%',
+  boxShadow: 24,
+};
+
+export default function BasicModal({
+  open,
+  setOpen,
+  title,
+  element,
+  previewImage,
+}) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'>
+        <Box sx={previewImage ? previewImageStyle : style}>
+          <Typography id='modal-modal-title' variant='h6' component='h2'>
+            {title}
+          </Typography>
+          {element}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+import { Avatar } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ReactS3 from 'react-s3';
+import { baseApi } from '../utils/utility';
+import axios from 'axios';
+import SnackBar from './SnackBar';
+import Compressor from 'compressorjs';
+window.Buffer = window.Buffer || require('buffer').Buffer;
+
+const config = {
+  bucketName: process.env.REACT_APP_BUCKET,
+  dirName: 'profile',
+  region: process.env.REACT_APP_REGION,
+  accessKeyId: process.env.REACT_APP_AWSAccessKeyId,
+  secretAccessKey: process.env.REACT_APP_AWSSecretAccessKey,
+};
+
+const UserProfile = ({ editAccess, setOpen }) => {
+  const [image, setImage] = useState();
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [severity, setSeverity] = useState('success');
+  const [snackBarMessage, setSnackBarMessage] = useState('');
+  const addImages = (event) => {
+    const images = [];
+    [...event.target.files].forEach((img) => {
+      const url = URL.createObjectURL(img);
+      images.push({ file: img, url });
+    });
+    const file = event.target.files[0];
+    new Compressor(file, {
+      quality: 0.4, // Adjust the quality value as per your requirement
+      success: (compressedResult) => {
+        // compressedResult has the compressed file.
+        // Use the compressed file to upload the images to your server.
+        compressedResult.name = localStorage.getItem('_id');
+        console.log(compressedResult.name);
+        ReactS3.uploadFile(compressedResult, config)
+          .then((data) => {
+            const patchData = { photoUrl: data.location };
+            axios
+              .patch(
+                `${baseApi}/api/users/${localStorage.getItem('_id')}`,
+                patchData,
+                {
+                  headers: {
+                    Authorization: `Bearer ${localStorage.getItem('Token')}`,
+                  },
+                }
+              )
+              .then((res) => {
+                setSeverity('success');
+                setSnackBarMessage(`Successfully updated profile picture`);
+                setOpenSnackBar(true);
+                localStorage.setItem('photoUrl', data.location);
+              })
+              .catch((err) => {
+                setSeverity('error');
+                setSnackBarMessage(err.message);
+                setOpenSnackBar(true);
+                console.error(err);
+              });
+          })
+          .catch((err) => {
+            setImage(false);
+            return console.error(err);
+          });
+      },
+    });
+
+    if (images && images[0]?.url) {
+      setImage(images[0].url);
+    }
+  };
+
+  const headerStyle = {
+    position: 'absolute',
+    top: '4%',
+    left: '7%',
+    color: 'black',
+    zIndex: '2',
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <SnackBar
+        message={snackBarMessage}
+        open={openSnackBar}
+        setOpen={setOpenSnackBar}
+        severity={severity}
+      />
+
+      <ArrowBackIcon sx={headerStyle} onClick={() => setOpen(false)} />
+
+      <div className='body-container'>
+        <div className='container'>
+          <div className='avatar'>
+            <label htmlFor='file-input'>
+              <Avatar
+                alt={localStorage.getItem('name')}
+                src={
+                  image ||
+                  localStorage.getItem('photoUrl') ||
+                  '/static/images/avatar/1.jpg'
+                }
+                sx={{
+                  width: 165,
+                  height: 165,
+                  borderRadius: '50%',
+                  backgroundColor: 'orange',
+                  fontSize: '4rem',
+                }}
+              />
+              <div className='plus-icon'>+</div>
+            </label>
+            <input
+              id='file-input'
+              type='file'
+              accept='image/jpg, image/jpeg, image/png, image/svg, image/svg+xml'
+              style={{ display: 'none' }}
+              onChange={addImages}
+            />
+          </div>
+          <div className='username'>
+            <h3>{localStorage.getItem('name')}</h3>
+          </div>
+          <div className='button' data-target='#home'>
+            <div className='button__icon'>
+              <i className='fa-solid fa-house'></i>
+            </div>
+            <div className='button__text'>Home</div>
+          </div>
+          <div className='button' data-target='#contact'>
+            <div className='button__icon'>
+              <i className='fa-solid fa-phone'></i>
+            </div>
+            <div className='button__text'>Contact</div>
+          </div>
+          <div className='button' data-target='#about'>
+            <div className='button__icon'>
+              <i className='fa-solid fa-user'></i>
+            </div>
+            <div className='button__text'>About</div>
+          </div>
+          <div className='button' data-target='#education'>
+            <div className='button__icon'>
+              <i className='fa-solid fa-user-graduate'></i>
+            </div>
+            <div className='button__text'>Education</div>
+          </div>
+        </div>
+        <div className='page' id='home'>
+          <div className='page__header'>
+            <div className='page__title'>Home</div>
+            <div className='page__close'>
+              <i className='fa-solid fa-xmark'></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
